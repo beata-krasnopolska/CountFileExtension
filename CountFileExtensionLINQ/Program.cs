7d9f8fa782239b9df037029f232bb751ea66d8d5
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2019-02-11 20:33:13 +0100

    Add project files.

---
+++ CountFileExtensionLINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CountFileExtensionLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program counts file extension and group it using LINQ");
+            string[] fileArray = { "aaa.frx", "bbb.TXT", "xyz.dbf", "abc.pdf", "aaaa.PDF", "xyz.frt", "abc.xml", "ccc.txt", "zzz.txt"};
+
+            var groupedFiles = fileArray.Select(file => Path.GetExtension(file).TrimStart('.').ToLower())
+                .GroupBy(z => z, (fExt, extCtr) => new
+                        {
+                            Extension = fExt,
+                            Count = extCtr.Count()
+                        });
+            foreach (var i in groupedFiles)
+            {
+                Console.WriteLine("{0} File(s) with {1} Extension ", i.Count, i.Extension);
+            }
+            Console.ReadKey();
+        }
+    }
+}
